4f9fff4b0abbc7dc31f4a3ed285fe771e360bcd7
Badr Choubai <badr.choubaidev@outlook.com>
2019-10-10 16:51:24 -0600

    Initial UI Setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <div id="app">
-    <h1>Book Stacks</h1>
-  </div>
+  <main id="app">
+    <header class="header">
+      <h1>Book stacks</h1>
+      <nav class="navigation-menu">
+        <a href="#">About</a>
+        <a href="#">GitHub</a>
+      </nav>
+    </header>
+    <div class="container">
+      <section id="book-stack-start">
+        <h2>Book Stack Start</h2>
+      </section>
+      <section id="book-stack-current">
+        <h2>Current Book</h2>
+      </section>
+      <section id="book-stack-finish">
+        <h2>Book Stack Finish</h2>
+      </section>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -17,6 +34,45 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+a {
+  text-decoration: none;
+  color: #333;
+}
+
+.header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.navigation-menu {
+  display: flex;
+  justify-content: space-around;
+  width: 150px;
+
+  a {
+    background: darken($color: #fff, $amount: 4);
+    padding: 16px;
+    margin: 4px;
+  }
+}
+
+.container {
+  position: absolute;
+  bottom: 0;
+  width: 98vw;
+
+  background: lightblue;
+  display: flex;
+  justify-content: space-around;
+  height: auto;
+}
+
+@media screen and (max-width: 700px) {
+  .container {
+    flex-direction: column;
+  }
 }
 </style>
